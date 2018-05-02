013382a69695bbcf24042d5df56fbfffc840e1b1
KimISeob <seobew@gmail.com>
2018-05-02 12:32:02 +0900

    solve joseph sequene with queue

---
+++ queue/11866.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+class Node{
+	public:
+		int value;
+		Node* next;
+		Node(int v){
+			this->value = v;
+		}
+};
+
+class Queue{
+	public:
+		Node* head;
+		Node* rear;
+		int length;
+		Queue(){
+			this->head = NULL;
+			this->rear = NULL;
+			this->length = 0;
+		}
+
+		void enqueue(int v){
+			Node* n = new Node(v);
+			if(is_empty()){
+				this->head = n;
+				this->rear = n;
+			}else{
+				this->rear->next = n;
+				this->rear = n;
+			}
+			this->length++;
+		}
+
+		int dequeue(){
+			if(is_empty()){
+				return -1;
+			}else{
+				int temp = this->head->value;
+				this->head = this->head->next;
+				this->length--;
+				return temp;
+			}
+		}
+
+		int front(){
+			return this->head->value;
+		}
+
+		bool is_empty(){
+			if(this->length<=0){
+				return true;
+			}else{
+				return false;
+			}
+		}
+};
+
+Queue q;
+
+void solve(int n, int m){
+	printf("<");
+	while(q.length>1){
+		for(int i = 0; i<m-1; i++){
+			int t = q.dequeue();
+			q.enqueue(t);
+		}
+		int x = q.dequeue();
+		printf("%d, ", x);
+	}
+	printf("%d>\n", q.front());
+}
+
+int main(){
+	int n,m;
+	scanf("%d %d", &n, &m);
+	for(int i = 1; i<n+1; i++){
+		q.enqueue(i);
+	}
+	solve(n,m);
+}
